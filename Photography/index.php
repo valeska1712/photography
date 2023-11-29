1860664d7b63cf198313f556ce216cab75f712a0
valeska1712 <101901658+valeska1712@users.noreply.github.com>
2023-11-29 12:06:45 +0300

    Add files via upload

---
+++ Photography/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Photographer's Portfolio</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <!-- Header content -->
+    </header>
+
+    <main>
+        <section class="gallery">
+            <div class="thumbnail">
+                <a href="gallery1.html">
+                    <img src="imgs/img1.jpg" alt="Gallery 1">
+                </a>
+                <h2>SPORTS</h2>
+                <p>Contains images about sports events like football.</p>
+            </div>
+            <div class="thumbnail">
+                <a href="gallery1.html">
+                    <img src="imgs/img2.jpg" alt="Gallery 1">
+                </a>
+                <h2>GAMING</h2>
+                <p>Contains high res images of videogames and gamers content.</p>
+            </div>
+            <div class="thumbnail">
+                <a href="gallery1.html">
+                    <img src="imgs/img3.jpg" alt="Gallery 1">
+                </a>
+                <h2>MOVIES</h2>
+                <p>Contains images about movies and the movie industry</p>
+            </div>
+            <!-- Repeat the above structure for other galleries -->
+        </section>
+    </main>
+
+    <footer>
+        <!-- Footer content -->
+    </footer>
+</body>
+</html>
